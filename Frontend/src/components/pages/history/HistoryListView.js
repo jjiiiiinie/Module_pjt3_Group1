643c0a76d5853560a49aac2882d43007a29b5d09
Sungho Bang <crosstreet74@gmail.com>
2021-09-09 18:06:02 +0900

    order data visible

--- Frontend/src/components/pages/history/HistoryListView.js
+++ Frontend/src/components/pages/history/HistoryListView.js
@@ -3,31 +3,38 @@ import { Link } from 'react-router-dom';
 
 export default function HistoryListView({ data }) {
 
+    const optionsState = 'packing';
+    const handleChange = () => {
+
+    }
+
     return (
-        <tr key={data.id}>
+        <tr key={data.orderId}>
             <td className="order-id">
+                {data.orderId}
             </td>
             <td className="order-user-id">
+                {data.userId}
             </td>
             <td className="order-product-name">
-                <Link to={`/productdetail/${data.id}`}>id:{data.id} / name:{data.name}</Link>
-            </td>
-            <td className="order-date">
-                {data.qty}
+                <Link to={`/productdetail/${data.productId}`}>{data.productId}</Link>
             </td>
             <td className="order-product-qty">
-                {/* {data.date} */}
+                {data.qty}
+            </td>
+            <td className="order-date">
+                {/* {data.qty} */}
             </td>
             <td className="order-status">
                 {/* {data.status} */}
             </td>
             <td className="order-status-select">
-                {/* <input type="select">
+                <select value={optionsState} onChange={handleChange}>
                     <option value="ordered">결제 완료</option>
                     <option value="packing">배송준비중</option>
                     <option value="shipped">배송 중</option>
                     <option value="delivered">배송 완료</option>
-                </input> */}
+                </select>
             </td>
         </tr>
     );
