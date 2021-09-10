6b548a2fe7d7b7f6cce420132e8e8f49f59cf98c
Sungho Bang <crosstreet74@gmail.com>
2021-09-10 14:04:47 +0900

    user/admin order history

--- Frontend/src/components/pages/history/HistoryListView.js
+++ Frontend/src/components/pages/history/HistoryListView.js
@@ -1,41 +1,63 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HistoryListView({ data }) {
+    const [status, setStatus] = useState(data.order_state);
 
-    const optionsState = 'packing';
-    const handleChange = () => {
 
+    const handleChange = (e) => {
+        console.log(e.target.value());
+        setStatus(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // 선택된 status 백엔드에 반영 요청
+        axios.put(`/order-service/orders/${data.orderId}`, { orderId: data.orderId, newStatus: status })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch()
     }
 
     return (
-        <tr key={data.orderId}>
-            <td className="order-id">
-                {data.orderId}
+        <tr key={data.order_id}>
+            <td className="order-id" aria-readonly>
+                {data.order_id}
             </td>
             <td className="order-user-id">
-                {data.userId}
+                {data.user_id}
             </td>
             <td className="order-product-name">
-                <Link to={`/productdetail/${data.productId}`}>{data.productId}</Link>
+                <Link to={`/productdetail/${data.product_id}`}>{data.product_id}</Link>
             </td>
             <td className="order-product-qty">
                 {data.qty}
             </td>
             <td className="order-date">
-                {/* {data.qty} */}
+                {data.created_at}
             </td>
-            <td className="order-status">
-                {/* {data.status} */}
-            </td>
+            
             <td className="order-status-select">
-                <select value={optionsState} onChange={handleChange}>
-                    <option value="ordered">결제 완료</option>
-                    <option value="packing">배송준비중</option>
-                    <option value="shipped">배송 중</option>
-                    <option value="delivered">배송 완료</option>
-                </select>
+                {
+                    sessionStorage.userId !== undefined && sessionStorage.userId.includes('admin') ?
+                        <select value={status} onChange={handleChange}>
+                            <option value="1">결제 완료</option>
+                            <option value="2">배송 중</option>
+                            <option value="3">배송 완료</option>
+                        </select> :
+                        `${data.order_state}`
+                }
             </td>
+            {
+                sessionStorage.userId !== undefined && sessionStorage.userId.includes('admin') ?
+                    <td className="order-status-confirm">
+                        <input type="submit" onSubmit={handleSubmit}>변경 반영</input>
+                    </td> :
+                    null
+            }
+           
         </tr>
     );
 }
--- Frontend/src/components/pages/history/HistoryTable.js
+++ Frontend/src/components/pages/history/HistoryTable.js
@@ -3,7 +3,7 @@ import HistoryListView from "./HistoryListView";
 import axios from 'axios';
 
 export default function HistoryTable() {
-
+  const [keyword, setKeyword] = useState('');
   const [historyDatas, setHistoryDatas] = useState([]);
   // 주문내역 데이터 GET
   // 사용자: 로그인된 사용자 정보와 함께 해당 사용자의 주문 내역 응답 GET 요청
@@ -29,20 +29,47 @@ export default function HistoryTable() {
     paymentPlan: ''
   */
 
+  // 검색 키워드 change state에 반영
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  }
+
+  // 검색 실행
+  const handleClick = () => {
+    // historyDatas에서 필터링
+    historyDatas.filter()
+
+  }
 
   useEffect(() => {
-    axios.get(`/order-service/${sessionStorage.userId}/orders`)
-      .then(res => {
-        console.log(res.data);
-        setHistoryDatas(res.data);
-      })
-      .catch()
+    // 세션 정보별 사용자/관리자 구분
+    if (sessionStorage.userId !== undefined && sessionStorage.userId.includes("admin")) {
+      axios.get(`/order-service/orders`)
+        .then(res => {
+          console.log(res.data);
+          setHistoryDatas(res.data);
+        })
+        .catch()
+    }
+    else {
+      axios.get(`/order-service/${sessionStorage.userId}/orders`)
+        .then(res => {
+          console.log(res.data);
+          setHistoryDatas(res.data);
+        })
+        .catch()
+    }
   }, []);
 
   return (
+    // 세션 정보 확인하여 선택적 렌더링 
     <div className="cart-main-area pt-90 pb-100">
       <div className="container">
         <h3 className="cart-page-title">Order History</h3>
+        <div className="mb-1 order-search-box row col-12">
+          <input className="col-3" type="text" onChange={handleChange} />
+          <button className="col-1" type="button" onClick={handleClick}>검색</button>
+        </div>
         <div className="row">
           <div className="col-12">
             <div className="table-content table-responsive cart-table-content">
@@ -55,7 +82,11 @@ export default function HistoryTable() {
                     <th>주문 수량</th>
                     <th>주문 일자</th>
                     <th>주문 상태</th>
-                    <th>주문 상태 변경</th>
+                    {
+                      sessionStorage.userId !== undefined && sessionStorage.userId.includes('admin') ?
+                        <th>주문 상태 변경</th> :
+                        null
+                    }
                   </tr>
                 </thead>
                 <tbody>
